1ca427f402e8d28b8f49a93f8b9419cd7174f5f8
Ananya Agarwal <ananyaagarwal1402@gmail.com>
2026-07-17 14:20:18 +0530

    created ex1.js file

---
+++ ex1.js
@@ -0,0 +1,19 @@
+// var a=10;
+// var a=20;
+// console.log("a=", a);
+
+// let a=10;
+// const b=25;
+// console.log("a=", a);
+// console.log("b=", b);
+// b=a++;
+// console.log("b=", b);
+
+// function sum(a, b)
+// {
+//     return a+b;
+// }
+const sum=(a,b)=>a+b;
+console.log("sum=", sum(10, 20));
+console.log("sum=", sum(20, 26));
+console.log("sum=", sum(13, 23));
